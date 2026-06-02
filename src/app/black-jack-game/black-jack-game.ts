23eb7d9c80dc64535b3db05e3e77d632570a296d
Jonas Abeln <jonas.abelnx@gmail.com>
2026-06-02 20:32:51 +0200

    added more logic for blackjack game and added cards(not complete)

--- src/app/black-jack-game/black-jack-game.ts
+++ src/app/black-jack-game/black-jack-game.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 interface DataItem {
@@ -13,16 +13,36 @@ interface DataItem {
   styleUrl: './black-jack-game.css',
 })
 export class BlackJackGame implements OnInit {
+  constructor(private cdr: ChangeDetectorRef) {}
+
   
   active_screen = "rules";
   
   StartGame(type: string) {
     this.active_screen = type;
+    this.startGame();
   }
   ngOnInit(): void {
-    this.engine();
   };
+startGame() {
+    this.playerHand = [];
+    this.dealerHand = [];
+    this.deck = [];
+    
+    this.cards();
+    this.shuffle();
+    
+    this.drawCard(this.playerHand);
+    this.drawCard(this.playerHand);
+    this.drawCard(this.dealerHand);
+    this.drawCard(this.dealerHand);
+    
+    this.rules();
+    this.playerValue = this.sum_card_value(this.playerHand);
+    this.dealerValue = this.sum_card_value(this.dealerHand);
 
+    this.cdr.detectChanges();
+}
   BET: boolean = true;
   NOD: number=8;
   DHS17: boolean = true;
@@ -30,11 +50,20 @@ export class BlackJackGame implements OnInit {
   DA: boolean = true;
   RA: boolean = false;
   SURR: boolean = true;
-  
-  engine() {
+
+  playerHand: any[] = [];
+  dealerHand: any[] = [];
+  deck: any[] = [];
+
+  playerValue: number=0;
+  dealerValue: number=0;
+
+  isShuffling: boolean = false;
+  private idCounter = 0;
+  cards() {
     
     //card objects
-    const card_patterns = ['Hearts', 'Diamonds', 'Spades', 'Clubs'];
+    const card_patterns = ['❤️', '♦️', '♠️', '♣️'];
     const card_value: Record<string, number> = {
       '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10,
       'J': 10, 'Q': 10, 'K': 10, 'A': 11
@@ -42,7 +71,7 @@ export class BlackJackGame implements OnInit {
 
     const values = Object.keys(card_value);
 
-    const deck = card_patterns.flatMap(card_pattern =>
+    const singleDeck = card_patterns.flatMap(card_pattern =>
       values.map(value => ({
         card_pattern,
         value,
@@ -50,35 +79,78 @@ export class BlackJackGame implements OnInit {
       }))
     );
 
-    // shuffle(FIsher-Yates algorithm)
-    function shuffle(array: any[]) {
-      for (let i = array.length-1;i>0;i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
-      }
-    }
-    shuffle(deck)
+  this.deck = [];
 
-    //Hands and deal
-    let playerHand: any[] = [];
-    let dealerHand: any[] = [];
-
-    function drawCard(hand: any[]) {
-      if (deck.length > 0) {
-        const card=deck.pop();
-        if (card) {
-          hand.push(card);
-        } else {
-          console.log("Deck is empty")
-        }
-      }
-    }
-
-
-
-
-
-
+  for (let i = 0; i < Number(this.NOD); i++) {
+    const deckWithIds = singleDeck.map(card => ({
+      ...card,
+      id: this.idCounter++
+    }));
+    this.deck.push(...deckWithIds);
+  }
 
   };
+
+  shuffle() {
+  // shuffle(Fisher-Yates algorithm)
+      for (let i = this.deck.length-1;i>0;i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [this.deck[i], this.deck[j]] = [this.deck[j], this.deck[i]];
+      }
+  };
+
+//Hands and deal
+drawCard(hand: any[]) {
+  console.log("Število kart v kupu:", this.deck.length);
+  if (this.isShuffling) return;
+  if (this.deck.length === 0) {
+    console.log("Deck is empty")
+    this.cards()
+    this.shuffle()
+    this.isShuffling = true;
+    
+    setTimeout(() => {
+      this.isShuffling = false;
+      this.cdr.detectChanges();
+      this.drawCard(hand);
+    },2000);
+
+    return;
+  }
+  const card=this.deck.pop();
+  if (card) {
+    hand.push(card);
+    this.playerValue = this.sum_card_value(this.playerHand);
+    this.dealerValue = this.sum_card_value(this.dealerHand);
+    this.cdr.detectChanges();
+  }
+};
+
+
+sum_card_value(hand:any []) {
+    let sum_value=0;
+    let aces=0;
+
+    for (const cards of hand) {
+      sum_value+=cards.points;
+      if (cards.value === 'A') aces+=1;
+    }
+
+    while (sum_value>21 && aces>0) {
+      sum_value-=10
+      aces-=1
+    }
+    return sum_value;
+};
+
+rules() {
+  const playerValue=this.sum_card_value(this.playerHand);
+  const dealerValue=this.sum_card_value(this.dealerHand);
+
+  if (playerValue > 21) {
+
+  }
+}
+
+
 };
